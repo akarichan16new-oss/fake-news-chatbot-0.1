96433c3047bec3ca23e0fe7d240a5d0f61758264
akarichan16new-oss <akarichan16new@gmail.com>
2026-03-12 21:08:38 +0000

    Update data_preprocessing.py
    Add data preprocessing script

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,1 +1,42 @@
+import pandas as pd
+import re
+import string
+from sklearn.model_selection import train_test_split
 
+def load_data(path):
+    df = pd.read_csv(path)
+    return df
+
+def clean_text(text):
+    text = text.lower()
+  
+    text = re.sub(r'http\S+', '', text)
+
+    text = re.sub(r'\d+', '', text)
+ 
+    text = text.translate(str.maketrans('', '', string.punctuation))
+
+    text = text.strip()
+
+    return text
+
+def preprocess_data(df):
+
+    df["text"] = df["text"].apply(clean_text)
+    X = df["text"]
+    y = df["label"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    return X_train, X_test, y_train, y_test
+
+if __name__ == "__main__":
+
+    df = load_data("data/fake_news_dataset.csv")
+
+    X_train, X_test, y_train, y_test = preprocess_data(df)
+
+    print("Training samples:", len(X_train))
+    print("Testing samples:", len(X_test))
